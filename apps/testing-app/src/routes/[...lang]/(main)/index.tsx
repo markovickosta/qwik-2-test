dba0101d2da1d0f62d6506f383a47256e80a5c82
Kosta <markovic.kosta@outlook.com>
2025-02-24 17:33:52 +0100

    Narrowed down the reproduction

--- apps/testing-app/src/routes/[...lang]/(main)/index.tsx
+++ apps/testing-app/src/routes/[...lang]/(main)/index.tsx
@@ -1,11 +1,9 @@
 import { Homepage } from '@qwik-2-test-workspace/homepage';
-import { component$, useSignal } from '@qwik.dev/core';
+import { component$ } from '@qwik.dev/core';
 import { type DocumentHead } from '@qwik.dev/router';
 
 export default component$(() => {
-	const isSearchMode = useSignal(false);
-
-	return <Homepage isActive={isSearchMode} />;
+	return <Homepage />;
 });
 
 export const head: DocumentHead = {
--- libs/app-specific/testing-app/feature-homepage/src/lib/homepage.tsx
+++ libs/app-specific/testing-app/feature-homepage/src/lib/homepage.tsx
@@ -1,58 +1,35 @@
-import { Signal, component$, isServer, useId, useSignal, useTask$ } from '@qwik.dev/core';
+import {
+	component$,
+	useSignal,
+	useStore,
+	useTask$,
+} from '@qwik.dev/core';
 
 import { Link } from '@qwik.dev/router';
 
-export const Homepage = component$((props: {isActive: Signal<boolean>;}) => {
-	const id = useId();
-	const windowBlur = useSignal<{
+export const Homepage = component$(() => {
+	const windowBlurSignal = useSignal<{
 		didHappen: boolean;
 		setupListeners: boolean | null;
-	  }>({ didHappen: false, setupListeners: null });
+	}>({ didHappen: false, setupListeners: null });
+	const windowBlurStore = useStore<{
+		didHappen: boolean;
+		setupListeners: boolean | null;
+	}>({ didHappen: false, setupListeners: null });
 
-	useTask$(({ track, cleanup }) => {
-		track(() => windowBlur);
-  
-		if (isServer) return;
-		if (
-		  windowBlur.value.setupListeners === null ||
-		  windowBlur.value.setupListeners === false
-		)
-		  return;
-  
-		windowBlur.value.setupListeners = false;
-  
-		const blurWindowListener = () => {
-		  windowBlur.value.didHappen = true;
-		};
-		const focusOutDocumentListener = (event: FocusEvent) => {
-		  windowBlur.value.didHappen = false;
-		  setTimeout(() => {
-			if (windowBlur.value.didHappen) {
-			  return;
-			}
-			if (props.isActive.value) {
-			  const globalSearchGroup = document.getElementById(id);
-			  if (!globalSearchGroup?.contains(event.relatedTarget as any)) {
-				props.isActive.value = false;
-			  }
-			}
-		  }, 0);
-		};
-  
-		window.addEventListener('blur', blurWindowListener);
-		document.addEventListener('focusout', focusOutDocumentListener);
-  
-		cleanup(() => {
-		  window.removeEventListener('blur', blurWindowListener);
-		  document.removeEventListener('focusout', focusOutDocumentListener);
-		});
-	  });
+	useTask$(({ track }) => {
+		track(() => windowBlurStore);
+	});
 
 	return (
-		<div id={id} class="flex flex-col">
+		<div class="flex flex-col">
 			<h1 class="text-2xl">Homepage 👋</h1>
-			<Link class="underline text-blue-600" href="/test">Go to Serbian test route</Link>
-			<Link class="underline text-blue-600" href="/en/test/">Go to English test route</Link>
+			<Link class="underline text-blue-600" href="/test">
+				Go to Serbian test route
+			</Link>
+			<Link class="underline text-blue-600" href="/en/test/">
+				Go to English test route
+			</Link>
 		</div>
 	);
 });
