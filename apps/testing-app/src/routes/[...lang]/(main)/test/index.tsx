6d6108bc89631fe249b8f593a1ade8da3a9d4183
Kosta <markovic.kosta@outlook.com>
2025-02-25 14:00:10 +0100

    Managed to reproduce qwik p1 is not a function

--- apps/testing-app/src/routes/[...lang]/(main)/test/index.tsx
+++ apps/testing-app/src/routes/[...lang]/(main)/test/index.tsx
@@ -1,9 +1,15 @@
+import { component$, useComputed$ } from '@qwik.dev/core';
+
 import { Link } from '@qwik.dev/router';
-import { component$ } from '@qwik.dev/core';
 import { inlineTranslate } from 'qwik-speak';
 
 export default component$(() => {
 	const t = inlineTranslate();
+
+	const productTitle = useComputed$(() => {
+		return 'Test title';
+	});
+
 	return (
 		<div class="flex flex-col">
 			<h1 class="text-2xl">Test route 👋</h1>
@@ -11,6 +17,15 @@ export default component$(() => {
 			<Link class="underline text-blue-600" href="/">
 				Go to homepage
 			</Link>
+			<br />
+			<img
+				src="https://cdn.builder.io/api/v1/image/assets%2Ffe30f73e01ef40558cd69a9493eba2a2%2Ffdc08238cb4d49d48d3a468308992e15?format=webp&width=2000"
+				alt={`${t('home.imageAlt.founded-product:')} ${productTitle.value}`}
+				decoding="async"
+				loading="lazy"
+				width={500}
+				height={275}
+			/>
 		</div>
 	);
 });
